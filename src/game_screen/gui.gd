3efe18657e6a4a4a7b1817e43e15ab34a731bdd2
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-12-12 00:08:06 +0200

    Show item artwork on the HUD.

--- src/game_screen/gui.gd
+++ src/game_screen/gui.gd
@@ -7,4 +7,13 @@ onready var _inventories: = [
 ]
 
 func update_inventory(index: int, inventory: Array) -> void:
-	_inventories[index].text = str(inventory)
+	var container: HBoxContainer = _inventories[index]
+	var pictures = container.get_children()
+	
+	for i in range(pictures.size()):
+		if i >= inventory.size():
+			pictures[i].visible = false
+			pictures[i].texture.region.position.x = 0
+			continue
+		pictures[i].visible = true
+		pictures[i].texture.region.position.x = inventory[i].sprite_frame * 8
