6cbfd36ce0480d72a160a7fc109d7c0a763f6d57
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-12-15 22:28:01 +0200

    Advance current day and save user data on night screen.

--- src/night_screen/night_screen.gd
+++ src/night_screen/night_screen.gd
@@ -2,8 +2,17 @@ extends Control
 
 
 onready var _fader = $"Fader"
+onready var _cash_label = $"CashLabel"
+onready var _title_label = $"TitleLabel"
+
+func _ready() -> void:
+	_cash_label.text = "$%.2f" % UserSaveData.current_cash
+	_title_label.text = tr("ui_night_title") % UserSaveData.current_day
+
 
 func _on_NextDayButton_pressed() -> void:
 	_fader.fade_out()
 	yield(_fader, "fade_out_completed")
+	UserSaveData.current_day += 1
+	UserSaveData.save_data()
 	get_tree().change_scene("res://game_screen/game_screen.tscn")
