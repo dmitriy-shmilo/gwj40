2815e07c26b6fa71c46f672f177ae8f39d576cfa
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-12-16 18:47:41 +0200

    Add cost to item resources.

--- src/data/item/item.gd
+++ src/data/item/item.gd
@@ -5,3 +5,5 @@ export(int) var id = -1
 export(int) var sprite_frame = 0
 export(bool) var unique = true
 export(String) var name = "item_unknown"
+export(float) var sell_cost = 1.0
+export(float) var restock_cost = 0.5
