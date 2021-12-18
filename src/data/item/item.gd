f26a9a072f63a6ab4b79e67bbda83ab292caf3be
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-12-18 10:44:52 +0200

    Define textures in item resources.

--- src/data/item/item.gd
+++ src/data/item/item.gd
@@ -3,6 +3,7 @@ class_name Item
 
 export(int) var id = -1
 export(int) var sprite_frame = 0
+export(Texture) var texture = null
 export(bool) var unique = true
 export(String) var name = "item_unknown"
 export(float) var sell_cost = 1.0
